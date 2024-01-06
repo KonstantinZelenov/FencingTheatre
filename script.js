88e2a6c570b3894d236f8561dba9731ee93b1aa8
Konstantin Zelenov <zelenovkonstantinv@gmail.com>
2024-01-06 10:41:35 +0400

    translate

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ el[2].innerHTML = "О школе";
 el[3].innerHTML = "Цены";
 el[4].innerHTML = "Персоналка";
 el[5].innerHTML = "Сплит занятие";
-el[6].innerHTML = "Групповое занятие";
+el[6].innerHTML = "Тренировка групп";
 el[7].innerHTML = "Съёмка видео";
 
 
@@ -143,3 +143,129 @@ const button = document.getElementsByClassName("form__button-button");
 button[0].innerHTML = "Отправьте заявку";
 
 };
+
+function rebootLang() {
+
+const headerHeading = document.getElementById("headerNameLink");
+headerHeading.innerHTML = "KONSTANTIN ZELENOV";
+
+const mainHeading = document.getElementsByTagName("h2");
+mainHeading[0].innerHTML = "KONSTANTIN ZELENOV";
+
+const descriptionHeading = document.querySelector(".description-title_p");
+descriptionHeading.innerHTML = "Fencing coach, stuntman, athlete";
+
+const headerList = document.getElementsByClassName("header__links-li");
+headerList[0].innerHTML = "About me";
+headerList[1].innerHTML = "Projects";
+headerList[2].innerHTML = "School";
+headerList[3].innerHTML = "Price";
+headerList[4].innerHTML = "Contacts";
+
+const headerDropList = document.getElementsByClassName ("header__dropdown-li");
+headerDropList[0].innerHTML = "Menu";
+headerDropList[1].innerHTML = "About me";
+headerDropList[2].innerHTML = "Projects";
+headerDropList[3].innerHTML = "School";
+headerDropList[4].innerHTML = "Price";
+headerDropList[5].innerHTML = "Contacts";
+
+const navList = document.getElementsByClassName("nav-translate");
+navList[0].innerHTML = "Instagram";
+navList[1].innerHTML = "Personal blog";
+navList[2].innerHTML = "VKontakte";
+navList[3].innerHTML = "More photos in albums";
+navList[4].innerHTML = "Telegram";
+navList[5].innerHTML = "Write me";
+navList[6].innerHTML = "Konstantin Zelenov, trainer, stuntman, athlete.";
+
+document.getElementById("contacts").innerHTML = "Contacts";
+
+const el = document.getElementsByTagName("h3");
+el[0].innerHTML = "About me";
+el[1].innerHTML = "About project";
+el[2].innerHTML = "About School";
+el[3].innerHTML = "Prices";
+el[4].innerHTML = "Personal training";
+el[5].innerHTML = "Split training";
+el[6].innerHTML = "Group training";
+el[7].innerHTML = "Shooting video";
+
+const aboutTrainerText = document.getElementsByClassName ("about-trainer__text-p");
+aboutTrainerText[0].innerHTML = "Hello, my name is Konstantin Zelenov and I am happy to share my story with you. I devoted the last 20 years of my life to fencing, honing my skills as an athlete, stuntman, theater and film actor.";
+aboutTrainerText[1].innerHTML = "I have twice become world champion in artistic fencing. Several times he was the champion of Russia and a multiple winner of competitions in artistic fencing.";
+aboutTrainerText[2].innerHTML = "Throughout my career, I have had the honor of coaching many students who have gone on to win prizes in artistic fencing competitions. My background in swordsmanship, stunts and acting allows me to bring a unique perspective to the training process.";
+aboutTrainerText[3].innerHTML = "Join me on this extraordinary journey as we explore complex footwork, weaponry and acting.";
+aboutTrainerText[4].innerHTML = "Whether you dream of climbing the championship podium or just want to experience the thrill of this ancient art form, I'm here to help you every step of the way. Contact me today to start an exciting adventure in the world of swordsmanship. Let's create something extraordinary together!";
+
+const aboutProjectText = document.getElementsByClassName ("about-project__text__p");
+aboutProjectText[0].innerHTML = "Step into a world where artistry and swordplay intertwine. Our video and theatrical projects in artistic fencing go beyond mere combat, through our carefully choreographed fight sequences, we create a visual symphony of movement and skill, captivating viewers with the beauty and intensity of the sport. We strive to ignite a spark within the hearts and minds of those who witness our performances.";
+aboutProjectText[1].innerHTML = "Every sword swing, every parry, and every dramatic pause is carefully crafted to convey a narrative that extends beyond the physical realm. We aim to touch the souls of our audience, to leave them pondering long after the final curtain falls or the last frame fades away.";
+aboutProjectText[2].innerHTML = "Join us on this artistic journey, where swords become brushes and the stage becomes our canvas. Together, we will create moments of pure magic, where the clash of steel and the power of storytelling converge to leave a lasting impact.";
+
+const videoContentDiscription = document.getElementsByClassName("video-content__discription__p");
+videoContentDiscription[0].innerHTML = "Welcome to our fencing school. Our fencing lessons will not only teach you the basics of technique such as attack, defense and movement, but will also help you develop flexibility, coordination and strength. Each student will receive attention and support in order to achieve maximum results in their development as a swordsman.";
+videoContentDiscription[1].innerHTML = "We also offer training in stunt art, where you can learn tricks, falls, and all the necessary skills to create spectacular fight scenes on screen or on stage.";
+videoContentDiscription[2].innerHTML = "We believe that swordsmanship and stuntmanship are not only physical skills, but also the art of self-expression and creativity.";
+
+const discription__price = document.getElementsByClassName("discription__priceList");
+
+discription__price[0].innerHTML = "Duration: 1 hour.";
+discription__price[1].innerHTML = "Price: 50 USD";
+discription__price[2].innerHTML = "Description: Hall rent is paid separately. In an hour we manage to warm up, learn fencing elements, and cool down.";
+discription__price[3].innerHTML = "Duration: 1.5 hours.";
+discription__price[4].innerHTML = "Price: 70 USD";
+discription__price[5].innerHTML = "Description: Hall rent is paid separately. In an hour and a half, we manage to warm up, learn fencing and basic acrobatic elements, and cool down.";
+discription__price[6].innerHTML = "Duration: 2 hours.";
+discription__price[7].innerHTML = "Price: 80 USD";
+discription__price[8].innerHTML = "Description: Hall rent is paid separately. For 2 hours we do a warm-up / hitch. We learn fencing, acrobatic elements. We discuss the idea of ​​the number and how to implement it.";
+discription__price[9].innerHTML = "Duration: 1 hour.";
+discription__price[10].innerHTML = "Price: 70 USD";
+discription__price[11].innerHTML = "Description: Hall rent is paid separately. In an hour we manage to warm up, learn fencing elements, and cool down.";
+discription__price[12].innerHTML = "Duration: 1.5 hours.";
+discription__price[13].innerHTML = "Price: 90 USD";
+discription__price[14].innerHTML = "Description: Hall rent is paid separately. Warm-up / cool-down, learn fencing and acrobatic elements, learn pair interaction.";
+discription__price[15].innerHTML = "Duration: 2 hours.";
+discription__price[16].innerHTML = "Price: 120 USD";
+discription__price[17].innerHTML = "Description: Hall rent is paid separately. Warm up / cool down. Fencing, pair acrobatics, elaboration of the idea of ​​the performance and its implementation. Number staging.";
+discription__price[18].innerHTML = "Duration: 2 hours.";
+discription__price[19].innerHTML = "Cost: 40 USD";
+discription__price[20].innerHTML = "Description: Hall rental is included in the price. In 2 hours we manage to warm up, learn fencing elements, and cool down.";
+discription__price[21].innerHTML = "Master class: 4 hours.";
+discription__price[22].innerHTML = "Price: 60 USD";
+discription__price[23].innerHTML = "Description: Hall rental is included in the price. We analyze complex elements, learn the schemes of group battles, analyze the methods of training and education.";
+discription__price[24].innerHTML = "Duration: 8 hours.";
+discription__price[25].innerHTML = "Price: 120 USD";
+discription__price[26].innerHTML = "Description: Hall rental is included in the price. Full accompaniment from the idea of ​​the performance to its implementation on stage. The plan from the first lesson to going on stage.";
+discription__price[27].innerHTML = "Duration: 1 hour.";
+discription__price[28].innerHTML = "Price: 100 USD";
+discription__price[29].innerHTML = "Description: Rent a location or transfer to a location separately. We're making a movie with you in the title role. The idea and costumes are discussed in advance.";
+discription__price[30].innerHTML = "Duration: 2 hours.";
+discription__price[31].innerHTML = "Price: 180 USD";
+discription__price[32].innerHTML = "Description: Rent a location or transfer to a location separately. Video shooting, material selection, simple editing, one edit at the request of the customer.";
+discription__price[33].innerHTML = "Duration: 4 or more hours.";
+discription__price[34].innerHTML = "Cost: 80 USD per hour.";
+discription__price[35].innerHTML = "Description: Location rental or transfer included (within the city). Video shooting, material selection, editing, color correction. One edit at the request of the customer.";
+
+const formChoice = document.getElementsByTagName("option");
+console.log("test", formChoice)
+formChoice[0].innerHTML = "Sword";
+formChoice[1].innerHTML = "Spear";
+formChoice[2].innerHTML = "Rapier";
+formChoice[3].innerHTML = "Sword and shield";
+formChoice[4].innerHTML = "Double sword";
+formChoice[5].innerHTML = "Staff";
+
+const legend = document.getElementsByTagName("legend");
+legend[0].innerHTML = "Contact me";
+
+const formLabel = document.getElementsByClassName("form__label");
+formLabel[0].innerHTML = "Name";
+formLabel[1].innerHTML = "Email";
+formLabel[2].innerHTML = "Lessons";
+formLabel[3].innerHTML = "Message";
+
+const button = document.getElementsByClassName("form__button-button");
+button[0].innerHTML = "Send your message";
+
+}
